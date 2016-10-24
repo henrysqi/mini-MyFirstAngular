8e8564fba91706faaa505485866854ee656c6954
Henry Qi <henryqi222@gmail.com>
2016-10-24 11:04:06 -0600

    added track by index

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('friendsList').controller('mainCtrl', function($scope){
-	$scope.friends = ["John", "Jane", "Moe", "Schmoe"];
+	$scope.friends = ["John", "Jane", "Moe", "Bob"];
 	$scope.addFriend = function(name){
 		$scope.friends.push(name);
 	}
